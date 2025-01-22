c3212df45f682eac3ede09817d0a47b72e013229
Shresthanirjala <nirjalas437@gmail.com>
2025-01-22 12:35:56 +0545

    deployement

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,8 @@
-
 import dotenv from "dotenv";
-dotenv.config({path:"../.env"});
+import path from "path";
+dotenv.config({ path: "../.env" });
 import cookieParser from "cookie-parser";
 import express from "express";
-
-
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -12,9 +10,9 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 import userRoutes from "./routes/user.routes.js";
 import { app, server } from "./socket/socket.js";
 
+const PORT = process.env.PORT || 5000;
 
-
-const PORT = process.env.PORT 
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -23,11 +21,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.get("/", (req, res) => {
-//   // root route http://localhost:5000
-//   res.send("hello world");
-// });
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
